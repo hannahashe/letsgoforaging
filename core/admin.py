037df169bab5d4771a6040e46b34c08ca1c97265
hannahashe <hannahashe13@gmail.com>
2026-05-15 14:13:07 +0100

    Register event model in admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Event
 
-# Register your models here.
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    """Admin settings for foraging events."""
+
+    list_display = (
+        "title",
+        "date",
+        "start_time",
+        "location",
+        "is_published",
+        "is_featured",
+    )
+    list_filter = ("is_published", "is_featured", "date")
+    search_fields = ("title", "short_description", "description", "location")
+    prepopulated_fields = {"slug": ("title",)}
+    ordering = ("date", "start_time")
