0ba93b1e82a66f61a9b3b22aba66d4e6614bbb88
hannahashe <hannahashe13@gmail.com>
2026-05-15 13:56:27 +0100

    URL paths and views for static content pages

--- core/views.py
+++ core/views.py
@@ -4,3 +4,18 @@ from django.shortcuts import render
 def home(request):
     """Display the website homepage."""
     return render(request, "core/home.html")
+
+
+def about(request):
+    """Display the about page."""
+    return render(request, "core/about.html")
+
+
+def contact(request):
+    """Display the contact page."""
+    return render(request, "core/contact.html")
+
+
+def gallery(request):
+    """Display the gallery page."""
+    return render(request, "core/gallery.html")
